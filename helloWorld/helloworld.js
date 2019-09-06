b1c7d7d85744542580cf990fb2e737e93df20c9e
dawimuhammad <muhamadhaddawi@gmail.com>
2019-09-06 15:31:44 +0700

    Always start with hello world!

---
+++ helloWorld/helloworld.js
@@ -0,0 +1,5 @@
+// console.log("hello world");
+function callMyName(name) {
+    console.log("Hello " + name);
+}
+callMyName('12');
---
+++ helloWorld/helloworld.ts
@@ -0,0 +1,7 @@
+// console.log("hello world");
+
+function callMyName(name: string) {
+    console.log("Hello " + name);
+}
+
+callMyName('12');
